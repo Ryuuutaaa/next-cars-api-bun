909b6cdaba9d2ef7fc502b91d875d852d98a05e5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-21 12:01:14 +0800

    add components SearchBar and CustomField

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,19 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width " id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the car you might like</p>
+        </div>
+
+        <div className="home__filters">
+          
+        </div>
+
+        <div className="home__filters-container"></div>
+      </div>
     </main>
   );
 }
